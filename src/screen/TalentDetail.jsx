e146fc12641f393e7b86636e269f0c53e111aa44
emirace <emmanuelakwuba57@gmail.com>
2024-05-22 07:50:30 +0100

    l

--- src/screen/TalentDetail.jsx
+++ src/screen/TalentDetail.jsx
@@ -212,7 +212,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     padding: 12,
-    marginBottom: 16,
+    marginBottom: 20,
   },
   downloadButtonText: {
     fontSize: 18,
